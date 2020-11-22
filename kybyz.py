44fbe424c23958b389d95a0eb8c77d9b33244159
John Comeau <jc@unternet.net>
2020-11-22 15:59:48 -0800

    doesn't really fix the problem but at least keeps from crashing

--- kybyz.py
+++ kybyz.py
@@ -220,7 +220,7 @@ def render(pagename):
     elif pagename.endswith('.html'):
         logging.debug('rendering %s as html', pagename)
         return read(pagename), 'text/html'
-    elif not pagename.endswith(('.png', '.ico', '.jpg', '.jpeg')):
+    elif not pagename.endswith(('.pdf', '.png', '.ico', '.jpg', '.jpeg')):
         logging.debug('rendering %s as plain text', pagename)
         return ('<div class="post">%s</div>' % cgi.escape(
             read(pagename)), 'text/plain')
