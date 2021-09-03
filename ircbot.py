76ac4bc7eab4bdc4efd2e9deed6f82694c665a0d
John Comeau <jc@unternet.net>
2021-09-02 17:21:17 -0700

    ircbot now lints 100%, haven't tested yet

--- ircbot.py
+++ ircbot.py
@@ -3,44 +3,73 @@
 IRC communications for server discovery
 '''
 # pylint: disable=multiple-imports
-import socket, pwd, os, threading, logging
+import socket, pwd, os, threading, logging, time
 
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
 IRCSERVER = 'irc.lfnet.org'
 PORT = 6667
+CHANNEL = '#kybyz'
 
-class IRC():
+class IRCBot():
     '''
     Implements IRC client
 
     see https://www.techbeamers.com/create-python-irc-bot/`
     also https://datatracker.ietf.org/doc/html/rfc2812
     '''
-    def __init__(self, channel='#kybyz', server=IRCSERVER,
-                 port=PORT, nickname=None, realname=None):
+    def __init__(self, server=IRCSERVER, port=PORT,
+                 nickname=None, realname=None):
         '''
         initialize the client
         '''
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = server
-        self.channel = channel
         self.nickname = nickname or pwd.getpwnam(os.geteuid()).pw_name
         self.realname = realname or pwd.getpwnam(os.geteuid()).pw_gecos
-        self.connection = self.connect(server, channel, port,
+        self.connection = self.connect(server, port,
                                        self.nickname, self.realname)
         self.terminate = False
-        while not self.terminate:
-            received = self.client.recv(2048).decode()
-            logging.info(received)
+        daemon = threading.Thread(target=self.monitor, name='ircbot_daemon')
+        daemon.daemon = True
+        daemon.start()
 
-    def connect(self, server, port, channel, nickname, realname):
+    def connect(self, server, port, nickname, realname):
         '''
         connect to the server and identify ourselves
         '''
         names = (nickname, realname)
-        self.client.connect((server, port))
+        connection = self.client.connect((server, port))
         self.client.send(('USER %s 0 * :%s\r\n' % names).encode())
-        logging.info('received: %s', self.irc.recv(2048).decode())
+        logging.info('received: %s', self.client.recv(2048).decode())
         self.client.send(('NICK %s\r\n' % nickname).encode())
-        logging.info('received: %s', self.irc.recv(2048).decode())
+        logging.info('received: %s', self.client.recv(2048).decode())
+        self.client.send(('JOIN %s\r\n' % CHANNEL).encode())
+        logging.info('received: %s', self.client.recv(2048).decode())
+        return connection
+
+    def monitor(self):
+        '''
+        wait for input. send a PONG for every PING
+
+        intended to run in a daemon thread
+
+        set ircbot.terminate to True in order to shut it down
+        '''
+        while not self.terminate:
+            received = self.client.recv(2048).decode()
+            logging.info(received)
+        logging.warning('IRC monitor terminated from launching thread')
+
+def test():
+    '''
+    run a bot from the command line, for testing
+    '''
+    try:
+        ircbot = IRCBot()
+        time.sleep(60)
+    except KeyboardInterrupt:
+        ircbot.terminate = True
+
+if __name__ == '__main__':
+    test()
