65652fc142e55a9696c6b319b82447d82ec61dc8
John Comeau <jc@unternet.net>
2021-09-05 10:21:28 -0700

    added docstrings and chmoded +x

--- post.py
+++ post.py
@@ -5,6 +5,14 @@ kybyz1 post
 from datetime import datetime, timezone
 
 class Post():
+    '''
+    encapsulation of kybyz post
+    '''
     @property
     def timestamp(self):
+        '''
+        untrusted timestamp.
+
+        will need blockchain for a trusted timestamp
+        '''
         return datetime.now(timezone.utc).isoformat()
