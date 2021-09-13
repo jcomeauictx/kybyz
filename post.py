68d2090b447b53cd3999626434766f5b67e7418c
John Comeau <jc@unternet.net>
2021-09-13 16:20:15 -0700

    working on getting html from json

--- post.py
+++ post.py
@@ -8,11 +8,19 @@ class Post():
     '''
     encapsulation of kybyz post
     '''
-    @property
-    def timestamp(self):
+    def __init__(self, **kwargs):
         '''
-        untrusted timestamp.
+        initialize instantiation from **dict
+        '''
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+        if not getattr(self, 'timestamp'):
+            self.timestamp = make_timestamp()
 
-        will need blockchain for a trusted timestamp
-        '''
-        return datetime.now(timezone.utc).isoformat()
+def make_timestamp():
+    '''
+    untrusted timestamp.
+
+    will need blockchain for a trusted timestamp
+    '''
+    return datetime.now(timezone.utc).isoformat()
