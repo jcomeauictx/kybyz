571ac601414855a3787006fc618eae5a9395dc5e
John Comeau <jc@unternet.net>
2021-09-14 14:29:28 -0700

    trying to generalize validate()

--- post.py
+++ post.py
@@ -8,14 +8,25 @@ class Post():
     '''
     encapsulation of kybyz post
     '''
-    def __init__(self, **kwargs):
+    def __init__(self, filename=None, **kwargs):
         '''
         initialize instantiation from **dict
         '''
+        self.filename = filename
         for key in kwargs:
             setattr(self, key, kwargs[key])
-        if not getattr(self, 'timestamp'):
+        if not getattr(self, 'timestamp', None):
             self.timestamp = make_timestamp()
+
+    def validate(self):
+        '''
+        make sure post contents fit the version given
+        '''
+        if not self.__doc__:
+            raise RuntimeError('Must not run with optimization')
+        classname = type(self).__name__.lower()
+        assert (getattr(self, 'type', None) == classname or
+                self.filename.endswith('.' + classname))
 
 def make_timestamp():
     '''
@@ -24,3 +35,6 @@ def make_timestamp():
     will need blockchain for a trusted timestamp
     '''
     return datetime.now(timezone.utc).isoformat()
+
+if __name__ == '__main__':
+    Post('testmeme.json').validate()
