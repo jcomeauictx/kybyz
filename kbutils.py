39e343378e94ff7d60ec38ce4611f37abb9d9174
John Comeau <jc@unternet.net>
2021-09-23 18:20:41 -0700

    added kbhash but it's not yet complete

--- kbutils.py
+++ kbutils.py
@@ -4,6 +4,9 @@ Kybyz utilities
 '''
 import logging
 from datetime import datetime, timezone
+from hashlib import sha256
+from base58 import b58encode
+from canonical_json import canonicalize
 
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
@@ -21,3 +24,12 @@ def make_timestamp():
     will need blockchain for a trusted timestamp
     '''
     return datetime.now(timezone.utc).isoformat()
+
+def kbhash(message):
+    '''
+    return base58 of sha256 hash of message, with prefix 'kbz'
+    '''
+    prefix = ''  # when added to 32-byte string produces 'kbz'
+    canonical = canonicalize(message)
+    hashed = sha256(canonical).digest()
+    return b58encode(prefix + hashed)
